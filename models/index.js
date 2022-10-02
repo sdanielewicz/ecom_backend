c2c7643d3f0858ad544b3f9fbf339550f548c4e1
sdanielewicz <sdanielewicz@gmail.com>
2022-10-02 14:20:05 -0700

    model relationships added

--- models/index.js
+++ models/index.js
@@ -6,18 +6,39 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.hasOne(Category, {
-
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
 });
 
-Product.belongsTo(Category, {
+// Category.belongsTo(Product, {
 
-});
+// });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: true,
+  },
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: true,
+  },
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
